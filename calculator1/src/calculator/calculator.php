5ee1457a505bc7d3110e287e4865f6cb2ca5f942
ahsanullahtamim <ahasantamim986@gmail.com>
2022-09-05 13:41:20 +0600

    first commit

---
+++ calculator1/src/calculator/calculator.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Pondit;
+
+class Calculator
+{
+    public $number1;
+    public $number2;
+
+    public function __construct($number1, $number2)
+    {
+        $this->number1 = $number1;
+        $this->number2 = $number2;
+    }
+
+    function add()
+    {
+        return $this->number1 + $this->number2;
+    }
+}
---
+++ calculator1/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'pondit_lab/calculator1',
+  ),
+  'versions' => 
+  array (
+    'pondit_lab/calculator1' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
